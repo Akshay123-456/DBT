49b6c0830c53b6ee526821d9c1bb88a2ece3b976
adeshmukh@information-asset.com <adeshmukh@information-asset.com>
2022-03-18 12:30:03 +0000

    new

--- models/sampleData.sql
+++ models/sampleData.sql
@@ -1,6 +1,1 @@
-with DBT_DATA as
-(
-    SELECT REGION as region, COUNTRY as country, ITEM_TYPE as item, UNITS_SOLD as units, TOTAL_PROFIT as profit
-    FROM "DBT_DATABASE"."PUBLIC"."DBT_DATA"
-)
 SELECT * FROM DBT_DATA
