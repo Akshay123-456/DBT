5a9753491fe4b0430f4c6741dc98ddb0925f325a
adeshmukh@information-asset.com <adeshmukh@information-asset.com>
2022-03-18 04:32:27 +0000

    commit with ref of model done

--- models/sampleData.sql
+++ models/sampleData.sql
@@ -1,2 +1,7 @@
 {{config(materialized="table")}}
-SELECT * FROM "DBT_DATABASE"."PUBLIC"."DBT_DATA"
+with DBT_DATA as
+(
+    SELECT REGION as region, COUNTRY as country, ITEM_TYPE as item, UNITS_SOLD as units, TOTAL_PROFIT as profit
+    FROM "DBT_DATABASE"."DBT_ADESHMUKH"."SAMPLEDATA"
+)
+SELECT * FROM DBT_DATA
