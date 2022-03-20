01a1a3fa8a8f2545d4b986c3863b2718f9b5f85b
adeshmukh@information-asset.com <adeshmukh@information-asset.com>
2022-03-20 17:54:42 +0000

    new commit

--- models/example/sampleData.sql
+++ models/example/sampleData.sql
@@ -1,6 +1,1 @@
-with New_Data as
-(
-    SELECT REGION as region, COUNTRY as country, ITEM_TYPE as item, UNITS_SOLD as units, TOTAL_PROFIT as profit
-    FROM "DBT_DATABASE"."PUBLIC"."DBT_DATA"
-)
-SELECT * FROM New_Data
+SELECT * FROM "DBT_DATABASE"."PUBLIC"."DBT_DATA"
