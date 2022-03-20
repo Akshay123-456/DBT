84913ff22f4899969637978e89a30c173a8317bd
adeshmukh@information-asset.com <adeshmukh@information-asset.com>
2022-03-20 18:32:10 +0000

    new

--- models/example/newData.sql
+++ models/example/newData.sql
@@ -1,9 +1,7 @@
 {% set payment_methods_query %}
 SELECT * FROM "DBT_DATABASE"."PUBLIC"."DBT_DATA"
 {% endset %}
-
 {% set results = run_query(payment_methods_query) %}
-
 {% if execute %}
 {# Return the first column #}
 {% set results_list = results.columns[0].values() %}
