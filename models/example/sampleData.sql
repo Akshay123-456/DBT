ef88afbda54304d57c5abb9d9cdc1ec6446705cf
adeshmukh@information-asset.com <adeshmukh@information-asset.com>
2022-03-21 04:48:24 +0000

    deleted one file and updated test case from schema

--- models/example/sampleData.sql
+++
@@ -1,6 +0,0 @@
-with New_Data as
-(
-    SELECT REGION as region, COUNTRY as country, ITEM_TYPE as item, UNITS_SOLD as units, TOTAL_PROFIT as profit
-    FROM "DBT_DATABASE"."PUBLIC"."DBT_DATA"
-)
-SELECT * FROM "DBT_DATABASE"."PUBLIC"."DBT_DATA"
